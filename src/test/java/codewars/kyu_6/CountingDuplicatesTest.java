19718131bf0ea75343b9cf79be62f9fea3dfcd72
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-04-14 12:42:35 +0200

    Do CountingDuplicates Tests

---
+++ src/test/java/codewars/kyu_6/CountingDuplicatesTest.java
@@ -0,0 +1,34 @@
+package codewars.kyu_6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class CountingDuplicatesTest {
+
+    @Test
+    void should_return_0_when_no_duplicates() {
+        //given
+        String string = "abcdefg";
+
+        //when
+        var result = CountingDuplicates.duplicateCount(string);
+
+        //then
+        assertThat(result, is(0));
+    }
+
+    @Test
+    void should_return_5_when_5_signs_duplicates() {
+        //given
+        String string = "Aaabb3333KKfdfjk";
+
+        //when
+        var result = CountingDuplicates.duplicateCount(string);
+
+        //then
+        assertThat(result, is(5));
+    }
+
+}
