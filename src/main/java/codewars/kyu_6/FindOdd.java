4baf7a39c32a4e297b597af162bbadb38780fb79
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-04-15 15:48:28 +0200

    solve FindOdd and add test cases

--- src/main/java/codewars/kyu_6/FindOdd.java
+++ src/main/java/codewars/kyu_6/FindOdd.java
@@ -11,13 +11,30 @@ package codewars.kyu_6;
 //        [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 //        [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
+import java.util.Arrays;
+
 public class FindOdd {
 
     public static void main(String[] args) {
-
+        System.out.println(findIt(new int[]{1,1,1,1,1,1,10,1,1,1,1}));
     }
 
-    static int findIt(int[] a) {
-        return 0;
+    static int findIt(int[] array) {
+        int result = 0;
+        var sorted = Arrays.stream(array).sorted().toArray();
+
+        for (int i = 0; i < sorted.length - 1; i++) {
+
+            if (sorted[i] == sorted[i + 1]) {
+                result++;
+            } else {
+                if (result % 2 == 0) {
+                    return sorted[i];
+                } else {
+                    result = 0;
+                }
+            }
+        }
+        return sorted[sorted.length - 1];
     }
 }
