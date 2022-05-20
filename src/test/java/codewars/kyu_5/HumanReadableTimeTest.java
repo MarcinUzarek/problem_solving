e4f156b83b2c98c2b9e17864904236b791ec9ac7
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-05-20 04:05:13 +0200

    Add HumanReadableTime and tests to it

---
+++ src/test/java/codewars/kyu_5/HumanReadableTimeTest.java
@@ -0,0 +1,18 @@
+package codewars.kyu_5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HumanReadableTimeTest {
+
+    @Test
+    void should_return_correct_time() {
+        assertEquals("00:00:00", HumanReadableTime.makeReadable(0));
+        assertEquals("00:00:05", HumanReadableTime.makeReadable(5));
+        assertEquals("00:01:00", HumanReadableTime.makeReadable(60));
+        assertEquals("23:59:59", HumanReadableTime.makeReadable(86399));
+        assertEquals("99:59:59", HumanReadableTime.makeReadable(359999));
+    }
+
+}
