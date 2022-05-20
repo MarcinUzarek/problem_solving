61166a5bdedef47e990805a79093c0f534697f74
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-05-20 21:13:06 +0200

    add TheMillionthFibonacci and tests to it

---
+++ src/main/java/codewars/kyu_3/TheMillionthFibonacci.java
@@ -0,0 +1,11 @@
+package codewars.kyu_3;
+
+import java.math.BigInteger;
+
+public class TheMillionthFibonacci {
+
+    public static BigInteger fib(BigInteger n) {
+
+        return null;
+    }
+}
