54bc5c0d02832e08b420fdaec10467fc8f3a5d02
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-05-24 20:15:11 +0200

    add BreakCamelCase and tests to it

---
+++ src/main/java/codewars/kyu_6/BreakCamelCase.java
@@ -0,0 +1,15 @@
+package codewars.kyu_6;
+
+//Complete the solution so that the function will break up camel casing, using a space between words.
+//
+//        Example
+//        "camelCasing"  =>  "camel Casing"
+//        "identifier"   =>  "identifier"
+//        ""             =>  ""
+
+public class BreakCamelCase {
+
+    public static String camelCase(String input) {
+        return null;
+    }
+}
---
+++ src/test/java/codewars/kyu_6/BreakCamelCaseTest.java
@@ -0,0 +1,20 @@
+package codewars.kyu_6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BreakCamelCaseTest {
+
+    @Test
+    void should_not_change_anything_for_one_word() {
+        assertEquals("camelcasingtest", BreakCamelCase.camelCase("camelcasingtest"));
+    }
+
+    @Test
+    void should_break_correctly_for_multi_words_string() {
+        assertEquals("camel Casing", BreakCamelCase.camelCase("camelCasing"));
+        assertEquals("camel Casing Test", BreakCamelCase.camelCase("camelCasingTest"));
+    }
+
+}
