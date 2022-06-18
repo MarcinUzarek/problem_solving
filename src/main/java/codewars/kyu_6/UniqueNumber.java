2d3302417fb8afc9a6ce1e47ea1e89e7fa2d99a3
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-06-18 16:57:56 +0200

    solve UniqueNumber

--- src/main/java/codewars/kyu_6/UniqueNumber.java
+++ src/main/java/codewars/kyu_6/UniqueNumber.java
@@ -1,9 +1,30 @@
 package codewars.kyu_6;
 
+//There is an array with some numbers. Try to find the non-repetitive one!
+//
+//        Kata.findUniq(new double[]{ 1, 1, 1, 2, 1, 1 }); // => 2
+//        Kata.findUniq(new double[]{ 0, 0, 0.55, 0, 0 }); // => 0.55
+
+//        It’s guaranteed that array contains at least 3 numbers.
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UniqueNumber {
 
     public static double findUniq(double[] arr) {
-        // Do the magic
-        return arr[0];
+
+        var list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        for (Double number : list) {
+            int start = list.size();
+            list.removeAll(List.of(number));
+
+            if (start - 1 == list.size()) {
+                return number;
+            }
+        }
+
+        return list.get(0);
     }
 }
--- src/test/java/codewars/kyu_6/UniqueNumberTest.java
+++ src/test/java/codewars/kyu_6/UniqueNumberTest.java
@@ -2,6 +2,9 @@ package codewars.kyu_6;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UniqueNumberTest {
@@ -12,5 +15,6 @@ class UniqueNumberTest {
         assertEquals(1, UniqueNumber.findUniq(new double[]{0, 1, 0}), precision);
         assertEquals(2, UniqueNumber.findUniq(new double[]{1, 1, 1, 2, 1, 1}), precision);
     }
+
 
 }
