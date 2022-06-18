e21d0e6cba1d73967e17d0eab425cb739df0f183
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-06-18 16:26:04 +0200

    add UniqueNumber and tests to it

---
+++ src/main/java/codewars/kyu_6/UniqueNumber.java
@@ -0,0 +1,9 @@
+package codewars.kyu_6;
+
+public class UniqueNumber {
+
+    public static double findUniq(double[] arr) {
+        // Do the magic
+        return arr[0];
+    }
+}
---
+++ src/test/java/codewars/kyu_6/UniqueNumberTest.java
@@ -0,0 +1,16 @@
+package codewars.kyu_6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniqueNumberTest {
+    private final double precision = 0.0000000000001;
+
+    @Test
+    void should_find_odd_number() {
+        assertEquals(1, UniqueNumber.findUniq(new double[]{0, 1, 0}), precision);
+        assertEquals(2, UniqueNumber.findUniq(new double[]{1, 1, 1, 2, 1, 1}), precision);
+    }
+
+}
