866764e7b939340b6826db2778c67315dc7f9240
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-06-26 08:47:59 +0200

    solve InvertValues

--- src/main/java/codewars/kyu_8/InvertValues.java
+++ src/main/java/codewars/kyu_8/InvertValues.java
@@ -7,10 +7,11 @@ package codewars.kyu_8;
 //        invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 //        invert([]) == []
 
+import java.util.Arrays;
+
 public class InvertValues {
 
     public static int[] invert(int[] array) {
-
-        return null;
+        return Arrays.stream(array).map(x -> -x).toArray();
     }
 }
