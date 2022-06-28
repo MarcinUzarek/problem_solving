0b7d266e5e4bc8ba6c3023715679d50eaee67953
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-06-28 08:51:41 +0200

    Add SumWithoutHighestAndLowest and tests to it

---
+++ src/test/java/codewars/kyu_8/SumWithoutHighestAndLowestTest.java
@@ -0,0 +1,25 @@
+package codewars.kyu_8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SumWithoutHighestAndLowestTest {
+
+    @Test
+    void should_sum_without_highest_and_lowest() {
+        assertEquals(16, SumWithoutHighestAndLowest.sum(new int[]{6, 2, 1, 8, 10}));
+        assertEquals(2, SumWithoutHighestAndLowest.sum(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    void should_return_zero_for_empty_input() {
+        assertEquals(0, SumWithoutHighestAndLowest.sum(new int[]{}));
+    }
+
+    @Test
+    void should_return_zero_for_input_with_less_than_3_elements() {
+        assertEquals(0, SumWithoutHighestAndLowest.sum(new int[]{1, 4}));
+    }
+
+}
