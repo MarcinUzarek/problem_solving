97f9ba291d91865106fe53e40a0061a0b099e495
MarcinUzarek <uzarek.marcin01@gmail.com>
2022-06-28 09:05:15 +0200

    add test cases to SumWithoutHighestAndLowestTest

--- src/test/java/codewars/kyu_8/SumWithoutHighestAndLowestTest.java
+++ src/test/java/codewars/kyu_8/SumWithoutHighestAndLowestTest.java
@@ -10,6 +10,7 @@ class SumWithoutHighestAndLowestTest {
     void should_sum_without_highest_and_lowest() {
         assertEquals(16, SumWithoutHighestAndLowest.sum(new int[]{6, 2, 1, 8, 10}));
         assertEquals(2, SumWithoutHighestAndLowest.sum(new int[]{1, 2, 3}));
+        assertEquals(5, SumWithoutHighestAndLowest.sum(new int[]{1, 2, 3, 3}));
     }
 
     @Test
@@ -21,5 +22,11 @@ class SumWithoutHighestAndLowestTest {
     void should_return_zero_for_input_with_less_than_3_elements() {
         assertEquals(0, SumWithoutHighestAndLowest.sum(new int[]{1, 4}));
     }
+
+    @Test
+    void should_return_zero_for_null_input() {
+        assertEquals(0, SumWithoutHighestAndLowest.sum(null));
+    }
+
 
 }
